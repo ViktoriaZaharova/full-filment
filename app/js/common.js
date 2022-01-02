bdf1586787a95218692bdb02d724fd2f0598fd9a
Viktoria <zaharova181114@gmail.com>
2022-01-02 22:21:22 +0300

    media

--- app/js/common.js
+++ app/js/common.js
@@ -16,5 +16,70 @@ $(document).ready(function () {
 $('.partner-slider').slick({
     slidesToShow: 6,
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                variableWidth: true,
+                arrows: false
+            }
+        }
+    ]
 });
+
+$('.btn-burger').on('click', function () {
+   $('.overlay').fadeIn();
+   $('.mobile-menu').fadeIn();
+});
+
+$('.btn-close-menu').on('click', function () {
+    $('.mobile-menu').fadeOut();
+    $('.overlay').fadeOut();
+});
+
+// slick active
+$(window).on('load resize', function() {
+    if ($(window).width() < 768) {
+        $('.advantages-slider:not(.slick-initialized)').slick({
+            infinite: true,
+            slidesToShow: 1,
+            variableWidth: true,
+            arrows: false,
+            centerMode: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+
+        $('.category-slider:not(.slick-initialized)').slick({
+            infinite: true,
+            slidesToShow: 1,
+            variableWidth: true,
+            arrows: false,
+            // centerMode: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+        $('.category-work-row:not(.slick-initialized)').slick({
+            infinite: true,
+            slidesToShow: 1,
+            variableWidth: true,
+            arrows: false,
+            // centerMode: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+    } else {
+        $(".advantages-slider.slick-initialized").slick("unslick");
+        $(".category-slider.slick-initialized").slick("unslick");
+        $(".category-work-row.slick-initialized").slick("unslick");
+    }
+});
+// slick active
